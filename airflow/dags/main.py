a9783dc1a1483c7b925df1f4d4723bcaabb24219
Anyub <118851510+ayoubnajjout@users.noreply.github.com>
2025-08-17 21:18:22 +0100

    first commit

---
+++ airflow/dags/main.py
@@ -0,0 +1,71 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from kafka import KafkaProducer
+import datetime
+import requests
+import json
+import logging
+import time
+
+default_args = {
+    'owner': 'airscholar',
+    'start_date': datetime.datetime(2025, 8, 17, 10, 00)
+}
+
+def get_data():
+    res = requests.get('https://randomuser.me/api/')
+    res.raise_for_status() 
+    res_json = res.json()['results'][0]
+    
+    user_name = res_json['name']["first"] + " " + res_json['name']["last"]
+    user_email = res_json['email']
+    user_gender = res_json['gender']
+    user_city = res_json['location']['city']
+    user_state = res_json['location']['state']
+    user_country = res_json['location']['country']
+    user_location = res_json['location']['street']['name'] + " " + str(res_json['location']['street']['number'])
+    user_phone = res_json['phone']
+    user_cell = res_json['cell']
+    
+    return {
+        "name": user_name,
+        "email": user_email,
+        "gender": user_gender,
+        "city": user_city,
+        "state": user_state,
+        "country": user_country,
+        "location": user_location,
+        "phone": user_phone,
+        "cell": user_cell
+    }
+
+def stream_data_to_kafka():
+    producer = None
+    start_time = time.time()
+    try:
+        # Adding api_version can help with compatibility issues
+        producer = KafkaProducer(bootstrap_servers='kafka:9093', api_version=(0, 10, 1))
+        while time.time() - start_time < 120:
+            data = get_data()
+            logging.info(f"Sending data to Kafka: {data}")
+            producer.send('user_data', json.dumps(data).encode('utf-8'))
+            producer.flush()
+            logging.info("Data sent to Kafka successfully.")
+            time.sleep(1)
+    except Exception as e:
+        logging.error(f"Error sending data to Kafka: {e}")
+        raise
+    finally:
+        if producer:
+            producer.close()
+            logging.info("Kafka producer closed.")
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data_to_kafka
+    )
